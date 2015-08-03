c3c6449139d2598b6d4bfab9ce4eefbb538ce0d3
Nick Sanders <nicksanders11@gmail.com>
2015-08-03 21:57:27 +0100

    Add test for zlib and fix session, all tests passing

--- lib/zlib.ex
+++ lib/zlib.ex
@@ -1,12 +1,20 @@
-defmodule Zlib do
+defmodule ExDjango.Zlib do
+
+  def compress(data) do
+    z = :zlib.open()
+    :zlib.deflateInit(z)
+    :zlib.deflate(z, data)
+    result = :zlib.deflate(z, << >>, :finish)
+    :zlib.deflateEnd(z)
+    result
+  end
 
   def decompress(data, wbits \\ 15) do
-    # z = :zlib:open()
-    # :zlib.inflateInit(z, wbits)
-    # result = :zlib.inflate(z, data)
-    # :zlib.inflateEnd(z)
-    # result
-    1
+    z = :zlib.open()
+    :zlib.inflateInit(z, wbits)
+    [result|_] = :zlib.inflate(z, data)
+    :zlib.inflateEnd(z)
+    result
   end
 
 end
