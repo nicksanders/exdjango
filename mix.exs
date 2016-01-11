854e38fe5fa56411c81d25ea305563899706c20e
Nick Sanders <nicksanders11@gmail.com>
2016-01-11 16:06:12 +0000

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExDjango.Mixfile do
 
   def project do
     [app: :exdjango,
-     version: "0.1.1",
+     version: "0.2.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
