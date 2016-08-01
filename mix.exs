f16042a77acca3e23eeec23249294bc1de3088d6
Nick Sanders <nicksanders11@gmail.com>
2016-08-01 10:44:27 +0100

    Fix tag out of sync with version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExDjango.Mixfile do
 
   def project do
     [app: :exdjango,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
